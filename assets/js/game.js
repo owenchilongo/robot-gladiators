082c5002db993110550deb7eeaaea3f13e3354ba
Owen <ochilongo@gmail.com>
2021-04-04 16:49:53 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,16 +4,37 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// // You can also log multiple values at once like this
+// console.log(playerName, playerAttack, playerHealth);
 
-// var enemyName = "Roborto"
-var enemyName = "Roborto";
+// var enemyName = "Roborto", "Amy Android", "Robo Trumble"
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames[3]);
+console.log(enemyNames.length);
+
+// Example of using the "for" loop
+// for(var i = 0; i < 3; i++) {
+//     console.log("apple" + i);
+// }
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     }
+
+// for (var i = 0; i < enemyNames.length; i++) {
+//   console.log(enemyNames[i]);
+//   console.log(i);
+//   console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
 // Fight Function
-var fight = function () {
+var fight = function (enemyName) {
   // This alerts palyers that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -29,9 +50,9 @@ var fight = function () {
     console.log(
       playerName +
         " attacked " +
-        enemyName +
+        enemyNames +
         ". " +
-        enemyName +
+        enemyNames +
         " now has " +
         enemyHealth +
         " health remaining."
@@ -47,7 +68,7 @@ var fight = function () {
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName +
+      enemyNames +
         " attacked " +
         playerName +
         ". " +
@@ -69,64 +90,29 @@ var fight = function () {
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave the fight
-     if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
 
-    // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
     }
     // if no (false), then ask the question again by running fight () again
     else {
-        fight();
+      fight();
     }
     window.alert(playerName + " has chosen to skip the fight!");
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
-
-  // //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable (50 - 10 =40)
-  // enemyHealth = enemyHealth - playerAttack;
-
-  // // Log a resulting message to the console so we know that it worked.
-  // console.log(
-  //     playerName +
-  //     " attacked " +
-  //     enemyName +
-  //     ". " +
-  //     enemyName +
-  //     " now has " +
-  //     enemyHealth +
-  //     " health remaining."
-  // );
-
-  // // check enemy's health...// In the fight function add the "check enemy's health" code, after you console.log that our enemy has been attacked
-  // if (enemyHealth <= 0) {
-  //     window.alert(enemyName + " has died!");
-  // } else {
-  //     window.alert(enemyName + " still has " + enemyHealth + " health left.");
-  // }
-
-  // // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable (100-12=88).
-  // playerHealth = playerHealth - enemyAttack;
-
-  // // Log a resulting message to the console so we know that it worked.
-  // console.log(
-  //     enemyName +
-  //     " attacked " +
-  //     playerName +
-  //     ". " +
-  //     playerName +
-  //     " now has " +
-  //     playerHealth +
-  //     " health remaining."
-  // );
-  // // check player's health // In the fight function add the "check player's health" code, after you console.log that our enemy has been attacked
-  // if (playerHealth <= 0) {
-  //     window.alert(playerName + " has died!");
-  // } else {
-  //     window.alert(playerName + " still has " + playerHealth + " health left.");
-  // }
 };
 
+// The "for" loop has to be after the fight () function expression becuase we're calling the fight () function within the loop. IF the function isn't declared before the function call, it will error.
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+  // console.log(enemyNames[i]);
+  // console.log(i);
+  // console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 // The fun fight function starts the game
-fight();
+// fight();
